badfb70b6a3c87bb171b5486312c7beae3177ab7
shanu <fgei.induction1@gmail.com>
2025-04-23 03:59:40 -0700

    updated conn

--- signup_process.php
+++ signup_process.php
@@ -1,5 +1,10 @@
 <?php
 // Prevent any unwanted output
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 ob_start();
 include('connection/conn.php');
 
