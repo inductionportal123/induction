5cdcca3781d385d8ea104c97d4ac16262026bb15
shanu <fgei.induction1@gmail.com>
2025-04-22 23:02:00 -0700

    All Files of Inductions Commitss Done

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include('connection/conn.php');
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>sdfsdfsdfsdfsdf</title>
+    <title>Welcome to FGEI (C/G) - Recruitment</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
