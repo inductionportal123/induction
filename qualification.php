b829f0846e5d2d962fa9acf700a97870e01dadca
shanu <fgei.induction1@gmail.com>
2025-04-24 02:52:27 -0700

    updated qualification

--- qualification.php
+++ qualification.php
@@ -328,11 +328,11 @@ if (isset($_SESSION['u_name'], $_SESSION['u_id'])) {
             <input type="hidden" name="dip_name_one" value="B.Ed/Equivalent">
         </td>
         <td>
-            <input type="text" name="dip_obt_one" id="dip_obt_one" value="<?php echo htmlspecialchars($profes_obtained_marks); ?>" class="w-full p-3 border rounded-md focus:ring-2 focus:ring-teal-500 required-field" required>
-        </td>
-        <td>
-            <input type="text" name="dip_total_one" id="dip_total_one" value="<?php echo htmlspecialchars($profes_total_marks); ?>" class="w-full p-3 border rounded-md focus:ring-2 focus:ring-teal-500 required-field" required>
-        </td>
+    <input type="number" name="dip_obt_one" id="dip_obt_one" value="<?php echo htmlspecialchars($profes_obtained_marks); ?>" min="0" step="any" class="w-full p-3 border rounded-md focus:ring-2 focus:ring-teal-500 required-field" required>
+</td>
+<td>
+    <input type="number" name="dip_total_one" id="dip_total_one" value="<?php echo htmlspecialchars($profes_total_marks); ?>" min="0" step="any" class="w-full p-3 border rounded-md focus:ring-2 focus:ring-teal-500 required-field" required>
+</td>
         <td>
             <input type="text" name="dip_board_one" id="dip_board_one" value="<?php echo htmlspecialchars($profes_board); ?>" class="w-full p-3 border rounded-md focus:ring-2 focus:ring-teal-500 required-field" required>
         </td>
