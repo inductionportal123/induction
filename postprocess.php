af9431ce35e7a416b4234a3b27c2706256c28e9c
shanu <fgei.induction1@gmail.com>
2025-04-23 21:21:48 -0700

    updated conn

--- postprocess.php
+++ postprocess.php
@@ -32,7 +32,7 @@ $list = '';
     
     
     if ($ppp == 1 && (is_null($dt) || $dt === '')) {
-        echo 'Professional Degree required for ' . htmlspecialchars($opendata1['name']) . ' Post. Please Complete Professional Degree Portion from Qualification';
+        echo 'A Professional Degree is required for the ' . htmlspecialchars($opendata1['name']) . ' position. Please complete the Professional Degree section in your Qualifications.';
     }
         else{
             
