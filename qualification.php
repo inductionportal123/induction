78221fa652b0b17e2e311125ce6a60a5fbe85caf
shanu <fgei.induction1@gmail.com>
2025-04-24 00:14:42 -0700

    updated qualification

--- qualification.php
+++ qualification.php
@@ -314,7 +314,7 @@ if (isset($_SESSION['u_name'], $_SESSION['u_id'])) {
                             <thead class="bg-teal-100">
                                 <tr>
                                     <th class="text-left text-teal-800">Sr.#</th>
-                                    <th class="text-left text-teal-800">Certificate/Diploma</th>
+                                    <th class="text-left text-teal-800">Degree</th>
                                     <th class="text-left text-teal-800">Obtained Marks</th>
                                     <th class="text-left text-teal-800">Total Marks</th>
                                     <th class="text-left text-teal-800">Board/University</th>
@@ -323,7 +323,7 @@ if (isset($_SESSION['u_name'], $_SESSION['u_id'])) {
                             <tbody>
                                 <tr>
                                     <th class="form-label">01</th>
-                                    <td><input type="text" name="dip_name_one" value="<?php echo htmlspecialchars($profes_certificate); ?>" class="w-full p-3 border rounded-md focus:ring-2 focus:ring-teal-500"></td>
+                                    <td><input type="text" name="dip_name_one" value="B.ed/Equivalent " class="w-full p-3 border rounded-md focus:ring-2 focus:ring-teal-500"></td>
                                     <td><input type="text" name="dip_obt_one" value="<?php echo htmlspecialchars($profes_obtained_marks); ?>" class="w-full p-3 border rounded-md focus:ring-2 focus:ring-teal-500"></td>
                                     <td><input type="text" name="dip_total_one" value="<?php echo htmlspecialchars($profes_total_marks); ?>" class="w-full p-3 border rounded-md focus:ring-2 focus:ring-teal-500"></td>
                                     <td><input type="text" name="dip_board_one" value="<?php echo htmlspecialchars($profes_board); ?>" class="w-full p-3 border rounded-md focus:ring-2 focus:ring-teal-500"></td>
